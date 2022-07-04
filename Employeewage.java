b0f628f1ff9e31fe5385fa185e4b98feff571d63
“Adithya <adithyasrinivas66@gmail.com>
2022-07-04 18:45:31 +0530

    employeecalculation

--- Employeewage.java
+++ Employeewage.java
@@ -21,4 +21,18 @@ package main.day8;
             }
             return 0;
         }
+
+        public class uc1empwage {
+            public double calculateEmpWage() {
+                int totalWorkingHour = 0;
+                int day = 0;
+
+                while (day < MAX_WORKING_DAY && (totalWorkingHour + WORKING_HOUR / 2) < MAX_WORKING_HOUR) {
+                    int isPresent = (int) (Math.random() * PRESENT_NUMBER_RANGE);
+                    totalWorkingHour = totalWorkingHour + getWorkingHour(isPresent);
+                    day++;
+                }
+            }
+        }
     }
+
