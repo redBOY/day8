ada45ea3f2c9b45797448df3def32ca5b94ad95d
“Adithya <adithyasrinivas66@gmail.com>
2022-07-04 19:25:28 +0530

    welcome to line comparison

--- Employeewage.java
+++ Employeewage.java
@@ -1,51 +1,48 @@
 package main.day8;
+public class Employeewage {
 
-    public class Employeewage {
+    final int PRESENT_NUMBER_RANGE = 3;
+    final int PRESENT = 1;
+    final int PART_TIME = 2;
+    final int WAGE_PER_HOUR = 20;
+    final int WORKING_HOUR = 8;
+    final int MAX_WORKING_DAY = 20;
+    final int MAX_WORKING_HOUR = 80;
 
-        final int PRESENT_NUMBER_RANGE = 3;
-        final int PRESENT = 1;
-        final int PART_TIME = 2;
-        final int WAGE_PER_HOUR = 20;
-        final int WORKING_HOUR = 8;
-        final int MAX_WORKING_DAY = 20;
-        final int MAX_WORKING_HOUR = 80;
+    public int getWorkingHour(int empPresent) {
+        switch (empPresent) {
+            case PRESENT:
+                return WORKING_HOUR;
 
-        public int getWorkingHour(int empPresent) {
-            switch (empPresent) {
-                case PRESENT:
-                    return WORKING_HOUR;
+            case PART_TIME:
+                return WORKING_HOUR / 2;
 
-                case PART_TIME:
-                    return WORKING_HOUR / 2;
-
-            }
-            return 0;
         }
-
-        public class uc1empwage {
-            public double calculateEmpWage() {
-                int totalWorkingHour = 0;
-                int day = 0;
-
-                while (day < MAX_WORKING_DAY && (totalWorkingHour + WORKING_HOUR / 2) < MAX_WORKING_HOUR) {
-                    int isPresent = (int) (Math.random() * PRESENT_NUMBER_RANGE);
-                    totalWorkingHour = totalWorkingHour + getWorkingHour(isPresent);
-                    day++;
-                }
-                double salary = totalWorkingHour * WAGE_PER_HOUR;
-                System.out.println("Employee monthly wage : $" + salary + " USD (total working hour : " + totalWorkingHour + ")");
-                System.out.println();
-                System.out.println("Total working day : " + day);
-                return salary;
-            }
-
-            public static void main(String[] args) {
-                System.out.println("Welcome to EmployeeWage Computation using OOPs!!!");
-                System.out.println();
-                Employeewage empWageBuilder = new Employeewage();
-                empWageBuilder.calculateEmpWage();
-            }
-        }
+        return 0;
     }
 
+    public double calculateEmpWage() {
+        int totalWorkingHour = 0;
+        int day = 0;
 
+        while (day < MAX_WORKING_DAY && (totalWorkingHour + WORKING_HOUR / 2) < MAX_WORKING_HOUR) {
+            int isPresent = (int) (Math.random() * PRESENT_NUMBER_RANGE);
+            totalWorkingHour = totalWorkingHour + getWorkingHour(isPresent);
+            day++;
+        }
+
+        double salary = totalWorkingHour * WAGE_PER_HOUR;
+        System.out.println("Employee monthly wage : $" + salary + " USD (total working hour : " + totalWorkingHour + ")");
+        System.out.println();
+        System.out.println("Total working day : " + day);
+        return salary;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to EmployeeWage Computation using OOPs!!!");
+        System.out.println();
+        Employeewage empWageBuilder = new Employeewage();
+        empWageBuilder.calculateEmpWage();
+    }
+
+}
---
+++ Linecomp.java
@@ -0,0 +1,9 @@
+package main.day8;
+
+public class Linecomp {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Line Comparison.");
+        int line1 = lengthOfLine(2, 1, 3, 2);
+
+    }
+}
